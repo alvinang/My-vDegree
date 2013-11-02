b0373cd04bc148c950a3f78b2297df261a0552de
Alvin Ang <alvinangbs@gmail.com>
2013-11-01 22:18:50 -0500

    Authenticated users redirect to dashboard index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Myvdegree::Application.routes.draw do
   devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
 
+  authenticated do
+    root to: 'dashboard#index', as: :authenticated_root
+  end
+
   root to: "home#index"
 end
